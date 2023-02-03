22e2fcb249a3973a92d90cdb3313c0075b2f8e23
Naresh Chudasama <112629859+nkc2887Dev@users.noreply.github.com>
2023-02-03 09:36:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  multiple: function (a, b) {
+    return a * b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  absolute: function (a) {
+    return Number.parseFloat(a).toFixed(2);
+  },
+};
---
+++ test.js
@@ -0,0 +1,13 @@
+let app = require("chudasama")
+
+const add = app.add(2,3)
+const sub = app.subtract(2,3)
+const mul = app.multiple(2,3)
+const dev = app.divide(20,5)
+const abs = app.absolute(2.000121323)
+
+console.log(add)
+console.log(sub)
+console.log(mul)
+console.log(dev)
+console.log(abs)
